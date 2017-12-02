6c8c853bc9ba0db29a26666f3d68ca410edfd466
Bryan Selner <dev@recoilvelocity.com>
2017-12-02 01:53:50 -0800

    Added test support for using a relative path in the INI since we don't know where the code has been installed.

--- tests/DirectoryTest.php
+++ tests/DirectoryTest.php
@@ -6,6 +6,9 @@ class DirectoryTest extends PHPUnit_Framework_TestCase
 {
     public function test_it_can_initialize_a_directory()
     {
+        // set a relative path variable we can use for the test files
+        putenv("CONFIG_TEST_ROOT=".__DIR__);
+
         $config = new Config\Config(__DIR__ . '/files');
 
         $this->assertInstanceOf(Config\Config::class, $config);
